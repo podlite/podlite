c7fedd6f58c67afad9257c5e45eab18583ebb4eb
Aliaksandr Zahatski <github.send@zag.ru>
2021-04-11 22:15:27 +0300

    collect base plugins

---
+++ packages/core/src/plugins/core.ts
@@ -0,0 +1,22 @@
+import { content } from 'pod6/built/helpers/handlers';
+export const core = {
+    ':image':{
+        toHtml:(writer)=>(node)=>{
+            if (typeof node !== "string" && 'type' in node && node.type === 'image')  {
+            writer.writeRaw(`<img`);
+            writer.writeRaw(` src="${node.src}"`)
+            if (node.alt) {
+                writer.writeRaw(` alt="${node.alt}"`)
+            }
+            writer.writeRaw(`/>`)
+            }
+        }
+        },
+    'image': { 
+        toHtml: content 
+    },
+    'root' : {
+        toHtml: content
+    }
+}
+export default core
