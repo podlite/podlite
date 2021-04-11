2e24c6792ad03bafb81573d0702aab5318d2d6d7
Aliaksandr Zahatski <github.send@zag.ru>
2021-04-11 22:03:47 +0300

    add service scripts

---
+++ packages/podlite-schema/scripts/scheme.ts
@@ -0,0 +1,22 @@
+import * as path from 'path'
+import * as TJS from 'typescript-json-schema'
+
+const settings: TJS.PartialArgs = {
+	required: true,
+    // noExtraProps: true,
+}
+const compilerOptions: TJS.CompilerOptions = {
+	strictNullChecks: true,
+}
+const basePath = path.join(__dirname, '../lib')
+
+const program = TJS.getProgramFromFiles(
+	[path.resolve('lib/index.d.ts'), path.resolve('lib/types.d.ts')],
+	compilerOptions,
+	basePath
+)
+
+const generator = TJS.buildGenerator(program, settings)
+export const Test = generator?.getSchemaForSymbol('Test')
+export const AstTree = generator?.getSchemaForSymbol('AstTree')
+export const PodliteDocument = generator?.getSchemaForSymbol('PodliteDocument')
