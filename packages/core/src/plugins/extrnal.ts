0a2b9016575d4e1b87a9448488d6fca81940dde7
Aliaksandr Zahatski <zag@cpan.org>
2021-04-18 13:30:09 +0300

    add =diagram

--- packages/core/src/plugins/extrnal.ts
+++ packages/core/src/plugins/extrnal.ts
@@ -1,7 +1,9 @@
 import {Plugin} from '@podlite/schema'
 import Image from '@podlite/image'
+import { plugin as Diagram } from '@podlite/diagram';
 const external = {
-    Image
+    Image,
+    Diagram
 }
 
 export default external
