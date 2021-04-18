7fe904daf5ea8bbb7c6f4c1ddf98b53c146a4106
Aliaksandr Zahatski <zag@cpan.org>
2021-04-18 20:12:36 +0300

    update deps

--- packages/core/src/plugins/extrnal.ts
+++ packages/core/src/plugins/extrnal.ts
@@ -2,7 +2,7 @@ import {Plugins} from '@podlite/schema'
 import Image from '@podlite/image'
 import { plugin as Diagram } from '@podlite/diagram';
 const external:Plugins = {
-    Image,
+    "Image":Image,
     Diagram
 }
 
