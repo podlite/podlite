81ddb719e54374b760c1970fb7e0223a95ee9421
Aliaksandr Zahatski <zag@cpan.org>
2021-11-14 16:12:39 +0300

    handle empty content

--- packages/podlite-image/src/index.ts
+++ packages/podlite-image/src/index.ts
@@ -4,27 +4,29 @@ import {mkBlockImage, mkBlockImageParams, Plugin} from '@podlite/schema'
 const Image:Plugin = {
     toAst: () => (node) => {
         if (typeof node !== "string" && 'type' in node && 'content' in node && node.type === 'block') {
-        const content = node.content[0]
-        if (typeof content !== "string" && 'location' in node && 'value' in content) {
-            // get link and alt text
-            const data = content.value
-            const altRegexp =  /\s*\[([^\[\]]*)\]\s*/
-            const altText = (data.match(altRegexp) || [,''])[1];
-            const link = altText ? data.replace(altRegexp, '') : data
-            const props:mkBlockImageParams = {
-                src:'',
-                location:node.location,
-                margin:node.margin
+            
+            const content = node.content[0]
+            if (content && typeof content !== "string" && 'location' in node && 'value' in content) {
+                // get link and alt text
+                const data = content.value
+                const altRegexp =  /\s*\[([^\[\]]*)\]\s*/
+                const altText = (data.match(altRegexp) || [,''])[1];
+                const link = altText ? data.replace(altRegexp, '') : data
+                const props:mkBlockImageParams = {
+                    src:'',
+                    location:node.location,
+                    margin:node.margin
+                }
+                if (link) {
+                    // clear all \n or spaces
+                    props.src = link.split(/\s+/)[0]
+                }
+                if (altText) {
+                    props.alt = altText
+                }
+                return mkBlockImage(props)
             }
-            if (link) {
-                // clear all \n or spaces
-                props.src = link.split(/\s+/)[0]
-            }
-            if (altText) {
-                props.alt = altText
-            }
-            return mkBlockImage(props)
-        }
+            return node
         }
 
      },
