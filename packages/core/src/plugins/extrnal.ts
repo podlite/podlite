a7f28f37dd2deca61af7ce204f1751dcade53b8c
Aliaksandr Zahatski <github.send@zag.ru>
2022-01-15 15:08:06 +0300

    add Toc

--- packages/core/src/plugins/extrnal.ts
+++ packages/core/src/plugins/extrnal.ts
@@ -1,9 +1,11 @@
 import {Plugins} from '@podlite/schema'
 import Image from '@podlite/image'
 import { plugin as Diagram } from '@podlite/diagram';
+import Toc from '@podlite/toc';
 const external:Plugins = {
     "Image":Image,
-    Diagram
+    Diagram,
+    Toc
 }
 
 export default external
