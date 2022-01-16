fc0eb51abcf4fb226354584f688893806903ef9b
Aliaksandr Zahatski <github.send@zag.ru>
2022-01-16 17:39:52 +0300

    implement Toc

--- packages/podlite-toc/src/index.tsx
+++ packages/podlite-toc/src/index.tsx
@@ -1,19 +1,7 @@
 import React from 'react'
-import {Plugin, Location, mkBlock, PodliteDocument, getFromTree, getTextContentFromNode, mkItemBlock} from '@podlite/schema'
+import {Plugin, Location, mkBlock, PodliteDocument, getFromTree, getTextContentFromNode, mkItemBlock, mkTocItem, mkTocList, mkToc, TocList, Toc, getNodeId} from '@podlite/schema'
 import { getTocPod, prepareDataForToc } from './helpers';
 import makeAttrs from 'pod6/built/helpers/config'
-let i = 0;
-const getNodeId = (node, ctx )=>{
-    const conf = makeAttrs(node, ctx)
-    if ( conf.exists('id') ) {
-        return  conf.getFirstValue('id')
-    }
-    return node.id
-}
-const setNodeId = (node, id )=>{
-     node.id = id
-     return node
-}
 
 export const plugin:Plugin =({
         toAst: (writer) => (node) => {
@@ -36,68 +24,34 @@ export const plugin:Plugin =({
             const content = getTextContentFromNode(node)
             const blocks = content.trim().split(/(?:\s*,\s*|\s+)/).filter(Boolean)
             const nodes = getFromTree(fulltree, ...blocks)
-            nodes.forEach(node => {
-                const id = getNodeId(node, ctx)
-                if (!id) {
-                    setNodeId(node, ++i)
-                }
-            })
             const tocTree = prepareDataForToc(nodes)
-            const podToc = getTocPod(tocTree)
-            return processor(podToc)
+            const createList = (items:any[], level):TocList=>{
+                const resultList = []
+                items.map(item => {
+                    const {level, node, content} = item
+                    // create new node for each item
+                    const text = getTextContentFromNode(node)
+                    //TODO: getNodeId should use ctx of node, but using {} instead
+                    const para = `L<${text}|#${getNodeId(node,{})}>`  
+                    const tocNode = processor(para)[0];
+                    resultList.push(mkTocItem(tocNode))
+                    if ( Array.isArray(content) && content.length > 0) {
+                        resultList.push(createList( content,level + 1) )
+                    };
+                })
+                return mkTocList(resultList, level)
+            }
+            const conf = makeAttrs(node, ctx)
+            const tocTitle = conf.getFirstValue('title')
+            const makeToc = (tocTree: any, title):Toc => {
+            
+               return  mkToc(createList(tocTree.content, 1), title)
+            }
+
+            return makeToc(tocTree, tocTitle)
         }
         },
-        toHtml: (writer) =>{
-            /** 
-            =Toc head, Diagram, Image , table
-            =for Toc title('Table of content')
-            head1,head2
-            =for Toc title('List of images')
-            Image
-            // head  => {name=>'head', level=>'*'}
-            head1 => {name=>'head', level=>1} + SEMANTIC
-            =item => {name=>'item', level=>'1'}
-
-            */
-            let fulltree
-            writer.on('start', (tree:PodliteDocument) => {
-                // TODO: tree should pass as third argument in pod6 release
-                fulltree = tree
-            })
-            return  (node) => {
-                // console.log(JSON.stringify(node,null,2))
-                //  console.log(JSON.stringify(fulltree,null,2))
-                const content = ( node.content[0] || {} ).value || ''
-                const blocks = content.trim().split(/(?:\s*,\s*|\s+)/).filter(Boolean)
-                if (blocks.length ) {
-                    // check if tree does not have any element of TOC
-                    const nodes = getFromTree(fulltree,...blocks);
-                    writer.writeRaw('<div id="toc" class="toc">')
-                    writer.writeRaw('<div id="toctitle">Table of Contents</div>')
-                    if (!nodes.length) {
-                        
-                    }
-                    // start TOC
-                    const r = getFromTree(fulltree,...blocks)
-                    let current_level = 1
-                    for( const item of r ) {
-                        const {name, level} = item
-                        const text = getTextContentFromNode(node)
-                        if (name === 'head') {
-                            current_level = level
-                            writer.writeRaw(`<div class="footnotes">`)
-                        } else if (name === 'item') {
-                            const level = current_level
-                            // writer.writeRaw(`<ul class="sectlevel${level}">`)
-                        }
-                    }
-                    // end TOC
-                    writer.writeRaw('</div>')
-                    // console.log(JSON.stringify(r[0], null, 2))
-                }
-                return node
-            }
-        }
+        
     })
 export default plugin
 
