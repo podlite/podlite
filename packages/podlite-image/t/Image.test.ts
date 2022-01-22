ac30fcf670ba595777e80b82ab05c7d73a9d8aa0
Aliaksandr Zahatski <zag@cpan.org>
2022-01-22 17:22:55 +0300

    refactor ast procession for =Image; add tests

--- packages/podlite-image/t/Image.test.ts
+++ packages/podlite-image/t/Image.test.ts
@@ -1,62 +1,75 @@
 // import  * as Image  from '../src';
-import { AstTree, isValidateError, PodliteDocument, validateAst} from '@podlite/schema'
-import { podlite as podlite_core } from 'podlite'
-import Image from '../src/index'
+import {
+  AstTree,
+  getFromTree,
+  isValidateError,
+  PodliteDocument,
+  validateAst,
+  validateAstTree,
+  validatePodliteAst,
+} from "@podlite/schema";
+import { frozenIds, podlite as podlite_core } from "podlite";
+import Image from "../src/index";
 
 const parse = (str: string): PodliteDocument => {
-    let podlite = podlite_core({ importPlugins: false }).use({ Image })
-    let tree = podlite.parse(str)
-    const asAst = podlite.toAst(tree)
-    return asAst
-}
+  let podlite = podlite_core({ importPlugins: false }).use({ Image });
+  let tree = podlite.parse(str);
+  const asAst = podlite.toAst(tree);
+  return asAst;
+};
 
 const parseToHtml = (str: string): string => {
-    let podlite = podlite_core({ importPlugins: false }).use({ Image })
-    let tree = podlite.parse(str)
-    const asAst = podlite.toAst(tree)
-    return podlite.toHtml(asAst).toString()
-}
+  let podlite = podlite_core({ importPlugins: false }).use({ Image });
+  let tree = podlite.parse(str);
+  const asAst = podlite.toAst(tree);
+  return podlite.toHtml(frozenIds()(asAst)).toString();
+};
 
 const cleanHTML = (html: string) => {
-    return html.replace(/\n+/g, '')
-}
+  return html.replace(/\n+/g, "");
+};
 
-const FIXTURES = [
-    [`=begin pod
-=Image https://example.com/test.png
-=end pod`,
-        "<img src=\"https://example.com/test.png\"/>",
-    '=Image url'
-    ],
+it("AST validate", () => {
+  const p = parse(`=Image test.png
+    short caption`);
+  const Image = getFromTree(p, "Image")[0];
+  const r = validateAstTree([Image]);
+  expect(r).toEqual([]);
+});
+it("=Image minimal", () => {
+  const pod = `=for Image 
+test`;
+  const html = parseToHtml(pod);
+  expect(html).toMatchInlineSnapshot(`
+    <div class="image_block"
+         id="id"
+    >
+      <img src="test"
+           alt="undefined"
+      >
+    </div>
+  `);
+});
 
-    [`=Image test`,
-        "<img src=\"test\"/>",
-    '=Image url'
-    ],
-
-    [`=Image alt text test`,
-        "<img src=\"test\" alt=\"alt text\"/>",
-    '=Image [alt] url'
-    ],
-
-    [`=for Image 
-test`,
-        "<img src=\"test\"/>"
-    ],
-
-]
-
-describe("ast validator", () => {
-    FIXTURES.map((i, idx) => test(`${i[2] || idx}`, () => {
-        const pod = i[0]
-        const result = parse(pod)
-        const r = validateAst(result, 'PodliteDocument')
-        const vres = isValidateError(r, result);
-        expect(r).toEqual([]);
-    }
-    ))
-})
-
-describe("run parser tests", () => {
-    FIXTURES.map((i, idx) => test(`${i[2] || idx}`, () => expect(cleanHTML(parseToHtml(i[0]))).toEqual(cleanHTML(i[1]))))
-})
+it("Image", () => {
+  const pod = `=for Image :id<111> :alt('testAlt') :caption('testCaption') :link('testLink')
+test.png
+`;
+  const html = parseToHtml(pod);
+  expect(html).toMatchInlineSnapshot(`
+    <div class="image_block"
+         id="111"
+    >
+      <a href="testLink">
+        <img src="test.png"
+             alt="testAlt"
+        >
+      </a>
+      <div class="caption">
+        <p>
+          testCaption
+        </p>
+      </div>
+    </div>
+  `);
+});
