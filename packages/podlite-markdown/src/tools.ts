d0f56ea17f62ec01c491449e1babef716f994b86
Aliaksandr Zahatski <github.send@zag.ru>
2022-10-23 19:02:41 +0200

    adjust location in blocks

--- packages/podlite-markdown/src/tools.ts
+++ packages/podlite-markdown/src/tools.ts
@@ -2,18 +2,31 @@ import  {unified}  from 'unified'
 import markdown  from 'remark-parse'
 import remarkGfm from 'remark-gfm'
 import toAny  from 'pod6/built/exportAny'
-import { AstTree, BlockImage, Location, mkBlock, mkFomattingCode, mkFomattingCodeL, mkVerbatim } from '@podlite/schema'
+import { AstTree, BlockImage, mkBlock, mkFomattingCode, mkFomattingCodeL, mkVerbatim, Location } from '@podlite/schema'
 import { mkRootBlock } from '@podlite/schema'
 import { mkImage } from '@podlite/schema'
-import { mkNode } from '@podlite/schema'
 import { mkCaption } from '@podlite/schema'
 import { mkFomattingCodeDelete } from '@podlite/schema'
 
-export const  md2ast = ( src, extraRules? ) :AstTree => {
+type Md2astArgs = { lineOffset?: number};
+export const  md2ast = ( src:string, {lineOffset }:Md2astArgs = {lineOffset:0} ) :AstTree => {
     // first convert mardown to ast 
     const md_tree = unified().use(markdown).use(remarkGfm).parse(src)
     // first pass : collect linkRefs
     let definitionMap = {}
+    const applyLineOffset = ((offset:number)=>(location:Location):Location=>{
+        const { start, end } = location
+        return {
+            "start": {
+                ...start,
+              "line": start.line + offset,
+            },
+            "end": {
+              ...end,
+              "line": end.line + offset,
+            }
+    }
+   })(lineOffset);
     toAny({processor:1})
     .use({'*:*':( writer, processor )=>( node, ctx, interator )=>{
         if (node.children) interator(node.children, { ...ctx})
@@ -26,7 +39,7 @@ export const  md2ast = ( src, extraRules? ) :AstTree => {
     const uast = []
     const  res = toAny({processor:1})
     .use({'*:*':( writer, processor )=>( node, ctx, interator )=>{
-        console.warn('[Podlite]')
+        console.warn('[Podlite:md2ast]- unhandled MD node')
         console.warn(JSON.stringify(node,null,2))
         if (node.children) interator(node.children, { ...ctx})
     }})
@@ -37,7 +50,7 @@ export const  md2ast = ( src, extraRules? ) :AstTree => {
         },
         ':heading' :  ( writer, processor )=>( node, ctx, interator )=>{
             const { children, position, ...attr} = node
-            return mkBlock({type:'block', level:node.depth,name:'head', location:position},  interator(children, ctx));
+            return mkBlock({type:'block', level:node.depth,name:'head', location:applyLineOffset(position)},  interator(children, ctx));
         },
         ':text' :  ( writer, processor )=>( node, ctx, interator )=>{
             const { children, position, ...attr} = node
@@ -52,11 +65,11 @@ export const  md2ast = ( src, extraRules? ) :AstTree => {
         ':listItem' :  ( writer, processor )=>( node, ctx, interator )=>{
             const savedListLevel = ctx['listLevel'] || 0
             const { children, position, ...attr} = node
-            return mkBlock({name: 'item', type:'block', level:ctx['listLevel'], location:position}, interator(children, { ...ctx, listLevel : savedListLevel , ordered: node.ordered}))
+            return mkBlock({name: 'item', type:'block', level:ctx['listLevel'], location:applyLineOffset(position)}, interator(children, { ...ctx, listLevel : savedListLevel , ordered: node.ordered}))
         },
         ':paragraph' :  ( writer, processor )=>( node, ctx, interator )=>{
             const { children, position, ...attr} = node
-            return mkBlock({type:'para', location:position, text: 'text', margin:"",}, interator(children, { ...ctx}))
+            return mkBlock({type:'para', location:applyLineOffset(position), text: 'text', margin:"",}, interator(children, { ...ctx}))
         },
         ':linkReference' : ( writer, processor )=>( node, ctx, interator )=>{
             const { children, position, ...attr} = node
@@ -85,39 +98,39 @@ export const  md2ast = ( src, extraRules? ) :AstTree => {
         },
         ':html': ( writer, processor )=>( node, ctx, interator )=>{
             const { children, position, ...attr} = node 
-            return mkBlock({type:'block', name:'Html', location:position},[mkVerbatim(node.value)]);
+            return mkBlock({type:'block', name:'Html', location:applyLineOffset(position)},[mkVerbatim(node.value)]);
         },
         ':code' : ( writer, processor )=>( node, ctx, interator )=>{
             const { children, position, lang, meta, ...attr} = node
             let config = []
             if (lang) { config.push({ name: 'lang', value:lang, type:"string"})}
             if (meta) { config.push({ name: 'meta', value:meta, type:"string"})} //  filled as is from markdown
-            return mkBlock({type:'block', name:'code', config, location:position},[mkVerbatim(node.value)]);
+            return mkBlock({type:'block', name:'code', config, location:applyLineOffset(position)},[mkVerbatim(node.value)]);
         },
         ':image':  ( writer, processor )=>( node, ctx, interator ):BlockImage=>{
             const { title, alt, url, position, ...attr} = node
             let config = []
-            return mkBlock({type:'block', name:'Image', config, location:position},[mkImage(url,alt),mkCaption([title])]);
+            return mkBlock({type:'block', name:'Image', config, location:applyLineOffset(position)},[mkImage(url,alt),mkCaption([title])]);
         },
         ':thematicBreak': ( writer, processor )=>( node, ctx, interator )=>{
             return null // TODO: add support for "thematic break"
         },
         ':blockquote' :  ( writer, processor )=>( node, ctx, interator )=>{
             const { children, position, ...attr} = node
-            return mkBlock({name:'nested', location:position}, interator(children, { ...ctx}))
+            return mkBlock({name:'nested', location:applyLineOffset(position)}, interator(children, { ...ctx}))
         },
         //table section
         ':table' :  ( writer, processor )=>( node, ctx, interator )=>{
             const { children, position, ...attr} = node
-            return mkBlock({name:'table', location:position}, interator(children, { ...ctx}))
+            return mkBlock({name:'table', location:applyLineOffset(position)}, interator(children, { ...ctx}))
         },
         ':tableRow' :  ( writer, processor )=>( node, ctx, interator )=>{
             const { children, position, ...attr} = node
-            return mkBlock({name:'table_row', location:position}, interator(children, { ...ctx}))
+            return mkBlock({name:'table_row', location:applyLineOffset(position)}, interator(children, { ...ctx}))
         },
         ':tableCell' :  ( writer, processor )=>( node, ctx, interator )=>{
             const { children, position, ...attr} = node
-            return mkBlock({name:'table_cell', location:position}, interator(children, { ...ctx}))
+            return mkBlock({name:'table_cell', location:applyLineOffset(position)}, interator(children, { ...ctx}))
         },
         ':delete': ( writer, processor )=>( node, ctx, interator )=>{
             const { children, position, ...attr} = node
@@ -126,7 +139,6 @@ export const  md2ast = ( src, extraRules? ) :AstTree => {
         ':break': ( writer, processor )=>( node, ctx, interator )=>{
             return null
         },
-        ...extraRules
     }).run(md_tree)
     return res.interator
 }
