ae5fcdbe306986cca67a298d89cb5959ffab3bcd
Aliaksandr Zahatski <zag@cpan.org>
2022-10-23 18:41:53 +0200

    update tests

--- packages/podlite-markdown/t/plugin.test.tsx
+++ packages/podlite-markdown/t/plugin.test.tsx
@@ -1,238 +1,205 @@
 import {
-  getFromTree,
+  isValidateError,
   PodliteDocument,
-  validatePodliteAst,
+  validateAstTree,
 } from "@podlite/schema";
-import { podlite as podlite_core } from "podlite";
-import { plugin } from "../src/index";
-import Image from "@podlite/image";
-import { frozenIds } from "podlite/src";
+import { md2ast } from "../src/tools";
+import { frozenIds, podlite as podlite_core, PodliteExport } from "podlite";
+import { PluginRegister } from "../src";
+import React from "react";
+import ReactDOM from "react-dom";
+import { makeTestPodlite } from "@podlite/to-jsx";
 
+const Podlite = makeTestPodlite(
+  podlite_core({ importPlugins: false }).use({
+    ...PluginRegister,
+  })
+);
+
+const root = document.body.appendChild(document.createElement("div"));
+
+function render(jsx) {
+  return ReactDOM.render(jsx, root);
+}
+
+afterEach(() => ReactDOM.unmountComponentAtNode(root));
+
+import { toTree, toHtml } from "pod6";
+const process = (src) => {
+  return md2ast(src);
+};
 export const parse = (str: string): PodliteDocument => {
   let podlite = podlite_core({ importPlugins: false }).use({
-    Toc: plugin,
+    ...PluginRegister,
   });
   let tree = podlite.parse(str);
   const asAst = podlite.toAstResult(tree);
-  return asAst.interator;
+  return frozenIds()(asAst.interator);
 };
 
-export const parseImage = (str: string): PodliteDocument => {
-  let podlite = podlite_core({ importPlugins: false }).use({
-    Toc: plugin,
-    Image,
-  });
-  let tree = podlite.parse(str);
-  const asAst = podlite.toAstResult(tree);
-  return asAst.interator;
-};
-
-const parseToHtml = (str: string): string => {
-  let podlite = podlite_core({ importPlugins: false }).use({
-    Toc: plugin,
-    Image,
-  });
-  let tree = podlite.parse(str);
-  const asAst = podlite.toAst(frozenIds()(tree));
-  return podlite.toHtml(asAst).toString();
-};
-
-const pod = `
-=begin pod
-=Toc head1 head3 item item2
-=head2 test2
-=end pod`;
-// =head1 test1 I<test> L<ddd | test >
-
-it("=Toc: toAst", () => {
-  const p = parse(pod);
-  // try to validate Formal AST
-  const r = validatePodliteAst(p);
+it("=Markdown: check line offset", () => {
+  const pod = `
+=para test
+=Markdown
+header1
+`;
+  const tree = parse(pod);
+  //   console.log(JSON.stringify(tree, null, 2));
+  const r = validateAstTree([tree]);
   expect(r).toEqual([]);
+  const errorDescribe = isValidateError(r, tree);
+  expect(parse(pod)).toMatchInlineSnapshot(`
+    Object {
+      "content": Array [
+        Object {
+          "type": "blankline",
+        },
+        Object {
+          "content": Array [
+            Object {
+              "content": Array [
+                Object {
+                  "type": "text",
+                  "value": "test
+    ",
+                },
+              ],
+              "location": Object {
+                "end": Object {
+                  "column": 1,
+                  "line": 3,
+                  "offset": 12,
+                },
+                "start": Object {
+                  "column": 1,
+                  "line": 2,
+                  "offset": 1,
+                },
+              },
+              "margin": "",
+              "text": "test
+    ",
+              "type": "para",
+            },
+          ],
+          "id": "id",
+          "location": Object {
+            "end": Object {
+              "column": 1,
+              "line": 3,
+              "offset": 12,
+            },
+            "start": Object {
+              "column": 1,
+              "line": 2,
+              "offset": 1,
+            },
+          },
+          "margin": "",
+          "name": "para",
+          "type": "block",
+        },
+        Object {
+          "config": Array [],
+          "content": Object {
+            "content": Array [
+              Object {
+                "content": Array [
+                  "header1",
+                ],
+                "id": "id",
+                "location": Object {
+                  "end": Object {
+                    "column": 8,
+                    "line": 4,
+                    "offset": 7,
+                  },
+                  "start": Object {
+                    "column": 1,
+                    "line": 4,
+                    "offset": 0,
+                  },
+                },
+                "margin": "",
+                "text": "text",
+                "type": "para",
+              },
+            ],
+            "id": "id",
+            "margin": "",
+            "name": "root",
+            "type": "block",
+          },
+          "id": "id",
+          "location": Object {
+            "end": Object {
+              "column": 1,
+              "line": 5,
+              "offset": 30,
+            },
+            "start": Object {
+              "column": 1,
+              "line": 3,
+              "offset": 12,
+            },
+          },
+          "margin": "",
+          "name": "Markdown",
+          "type": "block",
+        },
+      ],
+      "id": "id",
+      "margin": "",
+      "name": "root",
+      "type": "block",
+    }
+  `);
 });
 
-it("=para: parse to html", () => {
-  const pod = `=para head1 head3 item item2`;
-  expect(parseToHtml(pod)).toMatchInlineSnapshot(`
+it("=Markdown: jsx", () => {
+  const pod = `
+  =para test
+      =begin Markdown
+      header1
+      =head1 **ss**
+      ddd
+      =end Markdown
+    `;
+  render(<Podlite>{pod}</Podlite>);
+  expect(root.innerHTML).toMatchInlineSnapshot(`
+    <div id="id">
+      <p>
+        test
+      </p>
+    </div>
+    <p id="id">
+      header1
+    =head1
+      <strong>
+        ss
+      </strong>
+      ddd
+    </p>
     <p>
-      head1 head3 item item2
     </p>
   `);
 });
 
-it("=Toc: parse to html", () => {
-  const pod = `=Toc head1 head3 item item2`;
-  expect(parseToHtml(pod)).toMatchInlineSnapshot(`
-    <div classname="toc">
-      <ul class="toc-list listlevel1">
-      </ul>
-    </div>
-  `);
-});
-
-it("=Toc: head1 head2", () => {
-  const pod = `=Toc head1 head2
-=for head1 :id<123>
-Test head1
-`;
-  expect(parseToHtml(pod)).toMatchInlineSnapshot(`
-    <div classname="toc">
-      <ul class="toc-list listlevel1">
-        <li class="toc-item">
-          <p>
-            <a href="#123">
-              Test head1
-            </a>
-          </p>
-        </li>
-      </ul>
-    </div>
-    <h1 id="123">
-      Test head1
-    </h1>
-  `);
-});
-
-it("[check default list]=Toc", () => {
-  const pod = `=Toc
-=head1 head
-=head2 head
-=head3 head
-=head4 head
-=head5 head
-=head6 head
-    `;
-  expect(parseToHtml(pod)).toMatchInlineSnapshot(`
-    <div classname="toc">
-      <ul class="toc-list listlevel1">
-        <li class="toc-item">
-          <p>
-            <a href="#id">
-              head
-            </a>
-          </p>
-        </li>
-        <ul class="toc-list listlevel2">
-          <li class="toc-item">
-            <p>
-              <a href="#id">
-                head
-              </a>
-            </p>
-          </li>
-          <ul class="toc-list listlevel3">
-            <li class="toc-item">
-              <p>
-                <a href="#id">
-                  head
-                </a>
-              </p>
-            </li>
-            <ul class="toc-list listlevel4">
-              <li class="toc-item">
-                <p>
-                  <a href="#id">
-                    head
-                  </a>
-                </p>
-              </li>
-              <ul class="toc-list listlevel5">
-                <li class="toc-item">
-                  <p>
-                    <a href="#id">
-                      head
-                    </a>
-                  </p>
-                </li>
-                <ul class="toc-list listlevel6">
-                  <li class="toc-item">
-                    <p>
-                      <a href="#id">
-                        head
-                      </a>
-                    </p>
-                  </li>
-                </ul>
-              </ul>
-            </ul>
-          </ul>
-        </ul>
-      </ul>
-    </div>
-    <h1 id="id">
-      head
-    </h1>
-    <h2 id="id">
-      head
-    </h2>
-    <h3 id="id">
-      head
-    </h3>
-    <h4 id="id">
-      head
-    </h4>
-    <h5 id="id">
-      head
-    </h5>
-    <h6 id="id">
-      head
-    </h6>
-  `);
-});
-it.skip("=Toc Image Diagram1", () => {
-  const pod = `=for Toc :title<Table of Media>
-  Image Diagram 
-    =for Image :caption<Image caption> :id(1)
-    https://example.com.image.png
-    =Image https://example.com.image.png
-    
-    =for Diagram :caption<Diagram caption> :id(2)
-    User content
-    `;
-  const nodes = getFromTree(parseImage(pod), ":image");
-  console.log(JSON.stringify(nodes, null, 2));
-  console.log(parseToHtml(nodes));
-  // expect(parseToHtml(pod)).toMatchInlineSnapshot()
-});
-
-it("=Toc Image Diagram", () => {
-  const pod = `=for Toc :title<Table of Media>
-Image Diagram 
-  =for Image :caption<Image caption> :id(1)
-  https://example.com.image.png
-  =for Diagram :caption<Diagram caption> :id(2)
-  User content
+it("=Markdown: check 'delete' line element", () => {
+  const pod = `
+=begin Markdown
+asd ~~asdsd~~
+=end Markdown
   `;
-  expect(parseToHtml(pod)).toMatchInlineSnapshot(`
-    <div classname="toc">
-      <ul class="toc-list listlevel1">
-        <li class="toc-item">
-          <p>
-            <a href="#1">
-              Image caption
-            </a>
-          </p>
-        </li>
-        <li class="toc-item">
-          <p>
-            <a href="#2">
-              Diagram caption
-            </a>
-          </p>
-        </li>
-      </ul>
-    </div>
-    <div class="image_block"
-         id="1"
-    >
-      <img src="https://example.com.image.png"
-           alt="undefined"
-      >
-      <div class="caption">
-        <p>
-          Image caption
-        </p>
-      </div>
-    </div>
+  render(<Podlite>{pod}</Podlite>);
+  expect(root.innerHTML).toMatchInlineSnapshot(`
+    <p id="id">
+      asd
+      <del>
+        asdsd
+      </del>
+    </p>
+    <p>
+    </p>
   `);
 });
