2c5ddb0e1df161771ff80f02a24184b57d82ed40
Aliaksandr Zahatski <github.send@zag.ru>
2022-11-27 11:50:24 +0100

    add convert to md

--- packages/podlite-markdown/src/index.tsx
+++ packages/podlite-markdown/src/index.tsx
@@ -1,14 +1,15 @@
 import { Plugins } from '@podlite/schema';
 import {Plugin} from '@podlite/schema'
 import { md2ast } from './tools';
+import { subUse, wrapContent, emptyContent, content as nodeContent, setFn, handleNested } from 'pod6/built/helpers/handlers'
   
 export const plugin:Plugin =({
     toAst: (_, processor) => (node, ctx) => {
         if (typeof node !== "string" && 'type' in node && 'content' in node && node.type === 'block') {
-            
             const content = node.content[0]
             if (content && typeof content !== "string" && 'location' in node && 'value' in content) {
-                return md2ast(content.value) 
+                const lineOffset = node.location.start.line
+                return { ...node, content: md2ast(content, {lineOffset})  }
             }
             return node
         }
