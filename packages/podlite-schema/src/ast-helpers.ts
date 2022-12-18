465698b042506045018962aafa001452d9ac7fc9
Aliaksandr Zahatski <github.send@zag.ru>
2022-12-18 17:31:32 +0100

    update exports

--- packages/podlite-schema/src/ast-helpers.ts
+++ packages/podlite-schema/src/ast-helpers.ts
@@ -1,9 +1,10 @@
-import makeAttrs from 'pod6/built/helpers/config'
+import makeAttrsPod from 'pod6/built/helpers/config'
 
 export const getNodeId = (node, ctx )=>{
-    const conf = makeAttrs(node, ctx)
+    const conf = makeAttrsPod(node, ctx)
     if ( conf.exists('id') ) {
         return  conf.getFirstValue('id')
     }
     return node.id
 }
+export const makeAttrs = makeAttrsPod
