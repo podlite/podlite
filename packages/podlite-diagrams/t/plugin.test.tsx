3b2336fe4f202d7c662cbcddb2e71f1cf3bc19d3
Aliaksandr Zahatski <zag@cpan.org>
2022-12-18 17:31:15 +0100

    update tests

--- packages/podlite-diagrams/t/plugin.test.tsx
+++ packages/podlite-diagrams/t/plugin.test.tsx
@@ -2,13 +2,13 @@ import {
   PodliteDocument,
   validatePodliteAst,
   getFromTree,
-} from "@podlite/schema";
-import { podlite as podlite_core } from "podlite";
-import Diagram, { plugin as DiagramPlugin } from "../src/index";
-import { TestPodlite as Podlite } from "@podlite/to-jsx";
-import React from "react";
-import ReactDOM from "react-dom";
-import makeAttrs from "pod6/built/helpers/config";
+} from '@podlite/schema';
+import { podlite as podlite_core } from 'podlite';
+import Diagram, { plugin as DiagramPlugin } from '../src/index';
+import { TestPodlite as Podlite } from '@podlite/to-jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import makeAttrs from 'pod6/built/helpers/config';
 
 const parse = (str: string): PodliteDocument => {
   let podlite = podlite_core({ importPlugins: false }).use({
@@ -29,15 +29,15 @@ const parseToHtml = (str: string): string => {
 };
 
 const cleanHTML = (html: string) => {
-  return html.replace(/\n+/g, "");
+  return html.replace(/\n+/g, '');
 };
 
 const plugins = (makeComponent) => {
   return {
     Diagram: () => (node, ctx, interator) => {
       const conf = makeAttrs(node, ctx);
-      const caption = conf.exists("caption")
-        ? conf.getFirstValue("caption")
+      const caption = conf.exists('caption')
+        ? conf.getFirstValue('caption')
         : null;
       return makeComponent(
         ({ children, key }) => {
@@ -51,13 +51,13 @@ const plugins = (makeComponent) => {
           );
         },
         node,
-        interator(node.content, { ...ctx })
+        interator(node.content, { ...ctx }),
       );
     },
   };
 };
 
-const root = document.body.appendChild(document.createElement("div"));
+const root = document.body.appendChild(document.createElement('div'));
 
 function render(jsx) {
   return ReactDOM.render(jsx, root);
@@ -74,31 +74,31 @@ B-->C[fa:fa-ban forbidden]
 B-->D(fa:fa-spinner aaaaa);
 =end pod`;
 
-it("=Diagram: toAst", () => {
+it('=Diagram: toAst', () => {
   const p = parse(pod);
   // try to validate Formal AST
   const r = validatePodliteAst(p);
   expect(r).toEqual([]);
 });
 
-it("=Diagram: Error handle", () => {
+it('=Diagram: Error handle', () => {
   const p = parse(
     `=Diagram
 graph EROROROOROR
 A --- B
 B-->C[fa:fa-ban forbidden]
 B-->D(fa:fa-spinner aaaaa);
-`
+`,
   );
 
-  const diagram = getFromTree(p, "Diagram")[0];
-  expect("custom" in diagram).toBeTruthy();
+  const diagram = getFromTree(p, 'Diagram')[0];
+  expect('custom' in diagram).toBeTruthy();
 });
 
-it("=Diagrams: parse to html", () => {
+it('=Diagrams: parse to html', () => {
   expect(parseToHtml(pod)).toMatchInlineSnapshot(`""`);
 });
-it("=Diagrams: caption", () => {
+it('=Diagrams: caption', () => {
   const pod = `
 =begin Diagram :caption('Caption test')
     graph LR
@@ -110,7 +110,9 @@ it("=Diagrams: caption", () => {
 `;
   render(<Podlite plugins={plugins}>{pod}</Podlite>);
   expect(root.innerHTML).toMatchInlineSnapshot(`
-    <div class="diagram">
+    <div class="diagram"
+         id="id"
+    >
       <div class="mermaid">
       </div>
       <div class="caption">
@@ -120,11 +122,13 @@ it("=Diagrams: caption", () => {
   `);
 });
 
-it("accepts =Diagram", () => {
+it('accepts =Diagram', () => {
   render(<Podlite plugins={plugins}>{pod}</Podlite>);
   expect(root.innerHTML).toMatchInlineSnapshot(`
     <div id="id">
-      <div class="diagram">
+      <div class="diagram"
+           id="id"
+      >
         <div class="mermaid">
         </div>
       </div>
