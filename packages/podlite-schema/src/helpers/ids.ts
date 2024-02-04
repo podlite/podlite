3c0e955e8ffefd9e4c6724175f26987d0bc7596a
Aliaksandr Zahatski <github.send@zag.ru>
2024-02-04 17:14:03 +0100

    refactor helpers

---
+++ packages/podlite-schema/src/helpers/ids.ts
@@ -0,0 +1,75 @@
+import {BlockHead, getTextContentFromNode, makeTransformer} from ".."
+import { ParserPlugin } from ".."
+import { nanoid } from 'nanoid'
+import { PodNode } from '..'
+
+/**
+ * Clean ids from tree 
+ * @returns 
+ */
+
+export const cleanIds = (src = {skipChain: 0, podMode: 1}) =>( tree )=>{
+    const transformerBlocks = makeTransformer({
+      '*': (node, ctx, visiter) => {
+          if ('id' in node) {
+              if ( 'content' in node ) {
+                  node.content = visiter(node.content, ctx)
+              }
+              const {id, ...rest} = node
+          return { ...rest,  }
+          }
+          return node
+      }
+    })
+    return transformerBlocks(tree,{})
+  }
+
+/**
+ * Add set id to 'id' to each blocks
+ */
+export const frozenIds = (src = {skipChain: 0, podMode: 1}) =>( tree )=>{
+    const transformerBlocks = makeTransformer({
+      '*': (node, ctx, visiter) => {
+          if ('id' in node) {
+              if ( 'content' in node ) {
+                  node.content = visiter(node.content, ctx)
+              }
+              const {id, ...rest} = node
+            return { ...rest, id: 'id' }
+          }
+          return node
+      }
+    })
+    return transformerBlocks(tree,{})
+}
+   
+
+const middleware:ParserPlugin = () =>( tree )=>{
+  
+  const transformerBlocks = makeTransformer({
+    '*': (node, ctx, visiter) => {
+            const addIdField = (node:PodNode):PodNode => {
+                if (typeof node === 'object' && 'type' in node && node.type =='block') {
+                    if (node.name == 'caption') {
+                        return node
+                    } else if ( node.name == 'head') {
+                        return { ...node, id: getTextContentFromNode(node).trim() } as BlockHead
+                    } else {
+                        return { ...node, id: nanoid() } as PodNode
+                    }
+                }
+                return node
+            }
+            const processContent = (node:PodNode):PodNode => {
+                if (typeof node === 'object' && 'content' in node ) {
+                    return { ...node, content: visiter(node.content, ctx) }
+                }
+                return node
+            }
+            return processContent( addIdField(node) )
+        }
+  })
+  return transformerBlocks(tree,{})
+}
+export default middleware
+
