c00fa721755c688a1aec29f8eb4240af0af11fcf
Aliaksandr Zahatski <zag@cpan.org>
2024-05-23 09:31:20 +0200

    implement formulas

--- packages/podlite-formula/src/MathJax.tsx
+++ packages/podlite-formula/src/MathJax.tsx
@@ -13,11 +13,73 @@ export type ContextProps = {
 
 const MathJaxContext = createContext({})
 
+export function useTexSVG({
+  latex = '',
+  onSuccess = (html: HTMLElement) => {},
+  onError = (html: HTMLElement) => {},
+} = {}): [HTMLElement | null, { error: boolean; isLoading: boolean }] {
+  const mathJax: Window['MathJax'] | null = useContext(MathJaxContext) || null
+  const [html, setHtml] = useState<HTMLElement | null>(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const error = !!html?.outerHTML.match(/data-mjx-error/)
+
+  useEffect(() => {
+    async function setMathJaxHTML() {
+      const isReady = mathJax?.tex2svgPromise || (await mathJax?.loader?.ready?.())
+
+      if (isReady) {
+        try {
+          setIsLoading(true)
+          const mathJaxElement = await mathJax!.tex2svgPromise(latex)
+
+          setHtml(mathJaxElement)
+        } catch (e) {
+          console.error('Something went really wrong, if this problem persists then please open an issue', e)
+        } finally {
+          setIsLoading(false)
+        }
+      }
+    }
+
+    setMathJaxHTML()
+  }, [mathJax, latex])
+
+  useEffect(() => {
+    if (html && error) onError(html)
+    if (html && !error) onSuccess(html)
+  }, [html])
+
+  return [html, { error, isLoading }]
+}
+
 export type Tex2SVGProps = { [key: string]: any } & {
   latex: string
   onError?: (html: HTMLElement) => void
   onSuccess?: (html: HTMLElement) => void
   inline?: boolean
+}
+export const Tex2SVG: React.FC<Tex2SVGProps> = ({
+  latex = '',
+  onError = (html: HTMLElement) => {},
+  onSuccess = (html: HTMLElement) => {},
+  ...props
+}) => {
+  const ref = useRef<HTMLElement | null>(null)
+  const [html, { error }] = useTexSVG({ latex, onError, onSuccess })
+
+  useEffect(() => {
+    if (html && !error) {
+      Object.keys(props).map(key => html.setAttribute(key, props[key]))
+      ref.current?.appendChild(html)
+      return () => {
+        ref.current?.removeChild(html)
+      }
+    }
+
+    return () => {}
+  }, [props, html])
+
+  return <span ref={ref} />
 }
 
 export const Tex2SVGWithProvider: React.FC<Tex2SVGProps> = props => (
